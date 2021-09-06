9e93d2e3d98a26a64c7e32a4824eb8224887eb8d
semiexp <linthya.v@gmail.com>
2021-09-06 21:13:27 +0900

    Add IntegratedSolver::enumerate_valid_assignments and tests using it

--- src/integration.rs
+++ src/integration.rs
@@ -1,4 +1,4 @@
-use super::csp::{BoolExpr, BoolVar, Domain, IntExpr, IntVar, Stmt, CSP};
+use super::csp::{Assignment, BoolExpr, BoolVar, Domain, IntExpr, IntVar, Stmt, CSP};
 use super::encoder::{encode, EncodeMap};
 use super::norm_csp::NormCSP;
 use super::normalizer::{normalize, NormalizeMap};
@@ -53,6 +53,53 @@ impl IntegratedSolver {
             None => None,
         }
     }
+
+    /// Enumerate all the valid assignments of the CSP problem.
+    /// Since this function may modify the problem instance, this consumes `self` to avoid further operations.
+    pub fn enumerate_valid_assignments(mut self) -> Vec<Assignment> {
+        let mut bool_vars = vec![];
+        for i in 0..self.csp.vars.bool_var.len() {
+            bool_vars.push(BoolVar(i));
+        }
+        let mut int_vars = vec![];
+        for i in 0..self.csp.vars.int_var.len() {
+            int_vars.push(IntVar(i));
+        }
+
+        let mut ret = vec![];
+        loop {
+            let refutation_expr;
+
+            match self.solve() {
+                Some(model) => {
+                    let mut refutation = vec![];
+                    let mut assignment = Assignment::new();
+                    for &var in &bool_vars {
+                        let val = model.get_bool(var);
+                        assignment.set_bool(var, val);
+                        // TODO: the following fails:
+                        // refutation.push(Box::new(var.expr() ^ BoolExpr::Const(val)));
+                        if val {
+                            refutation.push(Box::new(!var.expr()));
+                        } else {
+                            refutation.push(Box::new(var.expr()));
+                        }
+                    }
+                    for &var in &int_vars {
+                        let val = model.get_int(var);
+                        assignment.set_int(var, val);
+                        refutation.push(Box::new(var.expr().ne(IntExpr::Const(val))));
+                    }
+                    refutation_expr = BoolExpr::Or(refutation);
+                    ret.push(assignment);
+                }
+                None => break,
+            }
+
+            self.add_expr(refutation_expr);
+        }
+        ret
+    }
 }
 
 pub struct Model<'a> {
@@ -81,7 +128,94 @@ impl<'a> Model<'a> {
 
 #[cfg(test)]
 mod tests {
+    use super::super::csp;
     use super::*;
+    use crate::util;
+
+    struct IntegrationTester {
+        original_constr: Vec<Stmt>,
+        solver: IntegratedSolver,
+        bool_vars: Vec<BoolVar>,
+        int_vars: Vec<(IntVar, Domain)>,
+    }
+
+    impl IntegrationTester {
+        fn new() -> IntegrationTester {
+            IntegrationTester {
+                original_constr: vec![],
+                solver: IntegratedSolver::new(),
+                bool_vars: vec![],
+                int_vars: vec![],
+            }
+        }
+
+        fn new_bool_var(&mut self) -> BoolVar {
+            let ret = self.solver.new_bool_var();
+            self.bool_vars.push(ret);
+            ret
+        }
+
+        fn new_int_var(&mut self, domain: Domain) -> IntVar {
+            let ret = self.solver.new_int_var(domain.clone());
+            self.int_vars.push((ret, domain));
+            ret
+        }
+
+        fn add_expr(&mut self, expr: BoolExpr) {
+            self.add_constraint(Stmt::Expr(expr));
+        }
+
+        fn add_constraint(&mut self, stmt: Stmt) {
+            self.original_constr.push(stmt.clone());
+            self.solver.add_constraint(stmt);
+        }
+
+        fn check(self) {
+            let mut bool_domains = vec![];
+            for _ in &self.bool_vars {
+                bool_domains.push(vec![false, true]);
+            }
+            let mut int_domains = vec![];
+            for (_, domain) in &self.int_vars {
+                int_domains.push(domain.enumerate());
+            }
+
+            let mut n_assignment_expected = 0;
+            for (vb, vi) in util::product_binary(
+                &util::product_multi(&bool_domains),
+                &util::product_multi(&int_domains),
+            ) {
+                let mut assignment = csp::Assignment::new();
+                for i in 0..self.bool_vars.len() {
+                    assignment.set_bool(self.bool_vars[i], vb[i]);
+                }
+                for i in 0..self.int_vars.len() {
+                    assignment.set_int(self.int_vars[i].0, vi[i]);
+                }
+                let is_sat_csp = self.is_satisfied_csp(&assignment);
+                if is_sat_csp {
+                    n_assignment_expected += 1;
+                }
+            }
+
+            let n_assignment = self.solver.enumerate_valid_assignments().len();
+            assert_eq!(n_assignment, n_assignment_expected);
+        }
+
+        fn is_satisfied_csp(&self, assignment: &csp::Assignment) -> bool {
+            for stmt in &self.original_constr {
+                match stmt {
+                    Stmt::Expr(e) => {
+                        if !assignment.eval_bool_expr(e) {
+                            return false;
+                        }
+                    }
+                    Stmt::AllDifferent(_) => todo!(),
+                }
+            }
+            true
+        }
+    }
 
     #[test]
     fn test_integration_simple_logic1() {
@@ -276,5 +410,65 @@ mod tests {
         let _ = model.get_int(a);
         let _ = model.get_int(b);
         let _ = model.get_int(c);
+    }
+
+    #[test]
+    fn test_integration_exhaustive_bool1() {
+        let mut tester = IntegrationTester::new();
+
+        let x = tester.new_bool_var();
+        let y = tester.new_bool_var();
+        let z = tester.new_bool_var();
+        let w = tester.new_bool_var();
+        tester.add_expr(x.expr().imp(y.expr() ^ z.expr()));
+        tester.add_expr(y.expr().imp(z.expr().iff(w.expr())));
+
+        tester.check();
+    }
+
+    #[test]
+    fn test_integration_exhaustive_linear1() {
+        let mut tester = IntegrationTester::new();
+
+        let a = tester.new_int_var(Domain::range(0, 2));
+        let b = tester.new_int_var(Domain::range(0, 2));
+        let c = tester.new_int_var(Domain::range(0, 2));
+        tester.add_expr((a.expr() + b.expr() + c.expr()).ge(IntExpr::Const(3)));
+
+        tester.check();
+    }
+
+    #[test]
+    fn test_integration_exhaustive_linear2() {
+        let mut tester = IntegrationTester::new();
+
+        let a = tester.new_int_var(Domain::range(0, 3));
+        let b = tester.new_int_var(Domain::range(0, 3));
+        let c = tester.new_int_var(Domain::range(0, 3));
+        let d = tester.new_int_var(Domain::range(0, 3));
+        tester.add_expr((a.expr() + b.expr() + c.expr()).ge(IntExpr::Const(5)));
+        tester.add_expr((b.expr() + c.expr() + d.expr()).le(IntExpr::Const(5)));
+
+        tester.check();
+    }
+
+    #[test]
+    fn test_integration_exhaustive_complex1() {
+        let mut tester = IntegrationTester::new();
+
+        let x = tester.new_bool_var();
+        let y = tester.new_bool_var();
+        let z = tester.new_bool_var();
+        let a = tester.new_int_var(Domain::range(0, 3));
+        let b = tester.new_int_var(Domain::range(0, 3));
+        let c = tester.new_int_var(Domain::range(0, 3));
+        tester.add_expr(
+            x.expr()
+                .ite(a.expr(), b.expr() + c.expr())
+                .eq(a.expr() - b.expr()),
+        );
+        tester.add_expr(a.expr().ge(y.expr().ite(b.expr(), c.expr())) ^ z.expr());
+
+        tester.check();
     }
 }
