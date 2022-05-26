74a603cd9ac322ca07d9a52aa01172decf059090
semiexp <linthya.v@gmail.com>
2022-05-26 19:40:49 +0900

    Fix wasm.rs

--- enigma_csp/src/wasm.rs
+++ enigma_csp/src/wasm.rs
@@ -9,7 +9,7 @@ fn run_solver(input: *const u8, len: i32) -> *const u8 {
     let result = csugar_cli::csugar_cli(&mut input, config::Config::default());
     unsafe {
         SHARED_ARRAY.clear();
-        SHARED_ARRAY.extend_from_slice(result.as_bytes());
+        SHARED_ARRAY.extend_from_slice(result.0.as_bytes());
         SHARED_ARRAY.push(0);
         SHARED_ARRAY.as_ptr()
     }
